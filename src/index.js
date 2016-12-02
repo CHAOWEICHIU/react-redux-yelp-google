2fbd8240c0245e122e92a959b63de3a1f867c679
chao wei chiu <chaoweichiu@gmail.com>
2016-12-02 14:22:46 -0500

    added redux dev-tool, logger

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers ,composeEnhancers(applyMiddleware(logger)))
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
