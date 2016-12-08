664c3f75f586a2b69fd810c2759251b0bf3bb8da
chao wei chiu <chaoweichiu@gmail.com>
2016-12-08 06:33:43 -0500

    server static file config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
     , path = require('path')
     , PORT = process.env.PORT || 8080
 
-app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'src')))
 
 app.get('*', (req,res)=>{
   res.sendFile(path.resolve(__dirname,'index.html'))
