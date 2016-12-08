ccd92df514df216e6128910756b65adc3adae3d9
chao wei chiu <chaoweichiu@gmail.com>
2016-12-08 06:57:31 -0500

    Edited README

---
+++ webpack.prod.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    './src/index'
+  ],
+
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  }
+}
