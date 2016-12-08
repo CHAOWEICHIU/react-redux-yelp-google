7fdceb4420fbeab782ae8c7fe5f5ce0fc40ec965
chao wei chiu <chaoweichiu@gmail.com>
2016-12-08 07:40:55 -0500

    Heroku config

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const express = require('express')
     , path = require('path')
     , PORT = process.env.PORT || 8080
 
-
 app.use(express.static('public'))
 app.get('/', (req, res)=> {
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
-
-
 
 app.listen(PORT)
 console.log('listening on port', PORT)
