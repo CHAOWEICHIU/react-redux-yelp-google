67285a3b69fe86e09c8c670b8d4a9df2c95c8aaa
chao wei chiu <chaoweichiu@gmail.com>
2016-12-08 07:21:45 -0500

    Heroku config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
+const path = require('path')
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
   },
   module: {
     loaders: [{
