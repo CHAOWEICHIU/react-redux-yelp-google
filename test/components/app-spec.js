29513772e9858541ad3908dbc8fe2d93b325cf12
chao wei chiu <chaoweichiu@gmail.com>
2016-12-11 11:41:04 -0500

    added test for app

--- test/components/app-spec.js
+++ test/components/app-spec.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
 import App from '../../src/components/app'
+import Header from '../../src/components/header'
+import Footer from '../../src/containers/footer'
 
-describe('App', ()=>{
-  it('pass',()=>{
-    
+describe.only('<App />', ()=>{
+  it('Has <Header /> ',()=>{
+    expect(
+      shallow(<App />).find(Header)
+    ).to.have.length(1)
+  })
+  it('Has <Footer /> ',()=>{
+    expect(
+      shallow(<App />).find(Footer)
+    ).to.have.length(1)
   })
 })
