b8f0a9cfe5e11c816122334a4663d8a04abd6c53
chao wei chiu <chaoweichiu@gmail.com>
2016-12-11 11:27:36 -0500

    added enzyme

---
+++ test/components/app-spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import App from '../../src/components/app'
+
+describe('App', ()=>{
+  it('pass',()=>{
+    
+  })
+})
